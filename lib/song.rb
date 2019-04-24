bd2df9888bd8c65fff6227deec51549576670ee6
Gregory Losco <gtlosco@gmail.com>
2019-04-24 00:38:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each {|genre|
-      if genre_count[genre] = genre
+      if genre_count[genre]
         genre_count[genre] += 1
       else genre_count[genre] = 1
     end
